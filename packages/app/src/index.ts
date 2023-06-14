9c919826755e93f45f8b9dba7f5654c83cc08137
Diego Gabriel <marcondesdddi@gmail.com>
2023-06-14 13:55:19 -0300

    init api with fastify

--- packages/app/src/index.ts
+++ packages/app/src/index.ts
@@ -1,3 +1,13 @@
-import { showMessage } from '@monorepo/common'
+import { app } from '@/main/config'
 
-showMessage()
+try {
+  const appPort = 3000
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+  app.listen({ port: appPort }, () => console.log(`API running http://localhost:${appPort}`))
+} catch (e) {
+  console.error(e)
+}
