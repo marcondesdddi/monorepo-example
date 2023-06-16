b7d8ef28e4bb360682ed7884fb36f42a1a5423b3
Diego Gabriel <marcondesdddi@gmail.com>
2023-06-16 11:48:44 -0300

    feat: add middleware helmet

--- packages/app/src/main/config/middlewares.ts
+++ packages/app/src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify"
 import cors from "@fastify/cors"
+import helmet from "@fastify/helmet"
 
 export const setupMiddleware = (app: FastifyInstance): void => {
   app.register(cors)
+  app.register(helmet)
 }
