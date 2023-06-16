6ddff6cc1c6e1259bf865b4a21bb6ae40cefd5d9
Diego Gabriel <marcondesdddi@gmail.com>
2023-06-16 12:16:29 -0300

    feat: add middleware formbody

--- packages/app/src/main/config/middlewares.ts
+++ packages/app/src/main/config/middlewares.ts
@@ -2,9 +2,11 @@ import { FastifyInstance } from "fastify"
 import cors from "@fastify/cors"
 import helmet from "@fastify/helmet"
 import healthCheck from "fastify-healthcheck"
+import formBody from "@fastify/formbody"
 
 export const setupMiddleware = (app: FastifyInstance): void => {
   app.register(cors)
   app.register(helmet)
   app.register(healthCheck)
+  app.register(formBody)
 }
