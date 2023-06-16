0a0259eaeea5adeaf290e6f1fc574b806852351e
Diego Gabriel <marcondesdddi@gmail.com>
2023-06-16 12:20:17 -0300

    feat: add middleware multipart

--- packages/app/src/main/config/middlewares.ts
+++ packages/app/src/main/config/middlewares.ts
@@ -3,10 +3,12 @@ import cors from "@fastify/cors"
 import helmet from "@fastify/helmet"
 import healthCheck from "fastify-healthcheck"
 import formBody from "@fastify/formbody"
+import multipart from "@fastify/multipart"
 
 export const setupMiddleware = (app: FastifyInstance): void => {
   app.register(cors)
   app.register(helmet)
   app.register(healthCheck)
   app.register(formBody)
+  app.register(multipart)
 }
